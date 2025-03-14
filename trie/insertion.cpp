2dd92fb70077c5be69a1c2e41f7dcafbfcb575db
ayaancdry <b23me1013@iitj.ac.in>
2025-03-14 15:13:53 +0530

    added insertion in trie

---
+++ trie/insertion.cpp
@@ -0,0 +1,39 @@
+/*
+Snippet to insert a new word in the trie
+*/
+#include <iostream>
+using namespace std;
+
+// Define TrieNode
+class TrieNode{
+public:
+        TrieNode* children[26];
+        bool isEndofWord;
+
+        TrieNode() {
+            isEndofWord = false;
+            for (int i=0; i<26; i++) {
+                children[i] = nullptr;
+            }
+        }
+};
+
+void insert(TrieNode* root, const string& key) {
+
+    // Initialise a curr pointer to the root. 
+    TrieNode* curr = root;
+
+    for (char c : key) {
+        int index = c - 'a';
+        // If the character is not present in the children array of the node pointed by curr,then create a newNode for the character at it's designated index. 
+        if (curr->children[index] == nullptr) {
+            TrieNode* newNode = new TrieNode();
+            // The newNode created corresponds to the character c of the key. It should point to the designated location
+            curr->children[c - 'a'] = newNode;
+        }
+        // After each iteration, move the curr pointer to next level. 
+        curr = curr->children[index];
+    }
+    // At the end of the word, mark the end of the word. 
+    curr->isEndofWord = true;
+}
